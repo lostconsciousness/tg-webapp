a8605e1b4c2cc7b9e0f882a05011659473d3d339
lostconsciousness <general2286@gmail.com>
2023-03-11 00:41:33 +0200

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-const tg = window.Telegram.WebApp;
 
 function App() {
 
-  const onClose = () =>{
-    tg.Close();
-  }
-
   return (
     <div className="App">
-        work
-        <button onClick={onClose}>close your mouth</button>
+        worked
     </div>
   );
 }
---
+++ src/components/Headers/Headers.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../Button/button";
+
+const Header = () =>{
+    const tg = window.Telegram.WebApp;
+    const onClose = () =>{
+        tg.Close();
+      }
+    return(
+        <div className={'header'}>
+            <Button onClick={onClose}>Close</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    )
+}
