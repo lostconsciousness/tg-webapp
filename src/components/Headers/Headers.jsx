910c90d305c5d270364fede93050bda36cddeab8
lostconsciousness <general2286@gmail.com>
2023-03-11 00:43:55 +0200

    sdfdsf

--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -8,7 +8,7 @@ const Header = () =>{
       }
     return(
         <div className={'header'}>
-            <Button onClick={onClose}>Close</Button>
+            <button onClick={onClose}>Close</button>
             <span className={'username'}>
                 {tg.initDataUnsafe?.user?.username}
             </span>
